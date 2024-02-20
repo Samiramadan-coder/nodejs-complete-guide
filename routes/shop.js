3f630fc7c31fad3fc4e2a52e93d8abc80765b8f3
Sami Ramadan <samyramadan.bts@gmail.com>
2024-02-20 19:59:14 +0200

    fix templates

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,17 @@
-const path = require("path");
+// const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 
 const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("Shop JS:", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
